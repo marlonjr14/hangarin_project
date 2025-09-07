6526680d2bac866797218e2aac5efaac3dc6f581
Lumiere14 <202380147@psu.palawan.edu.ph>
2025-09-07 23:58:32 +0800

    Update settings.py

--- hangarin_project/settings.py
+++ hangarin_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nv(0_j6+7*u@v^3xu=$%%8giw2%atbdx#isadtbkefl85trjlo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = ["marlonjr.pythonanywhere.com", "127.0.0.1", "localhost"]
 
